6dc4f69ee5e3ea15520722a0e2c09bfa02b319a6
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-13 16:36:32 +0100

    header and main (first push)

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="header">
+        <div>
+            <img :src="imageLink" alt="">
+        </div>
+       
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a class="active" href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data (){
+        return{
+            imageLink: require("../assets/img/dc-logo.png")
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        width: 80%;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    ul {
+        display: flex;
+        font-size: 0.6em;
+        list-style-type: none;
+        li a{
+            display: block;
+            padding: 2em 1em;
+            text-decoration: none;
+            color: black;
+            &:hover,
+            &:active,
+            &.active {
+                color: #005ebd;
+                border-bottom: 3px solid #005ebd;
+            }
+        }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+      <div class="main-content">
+          <h2>-->Content Goes Here</h2>
+      </div>
+      <div class="main-end-menu">
+          <ul>
+              <li><a href=""><img src="../assets/img/buy-comics-digital-comics.png" alt=""><span>DIGITAL COMICS</span></a></li>
+              <li><a href=""><img src="../assets/img/buy-comics-merchandise.png" alt=""><span>DC MERCHANDISE</span></a></li>
+              <li><a href=""><img src="../assets/img/buy-comics-subscriptions.png" alt=""><span>SUBSCRIPTION</span></a></li>
+              <li><a href=""><img class="location-logo" src="../assets/img/buy-comics-shop-locator.png" alt=""><span>COMIC SHOP LOCATION</span></a></li>
+              <li><a href=""><img src="../assets/img/buy-dc-power-visa.svg" alt=""><span>DC POWER VISA</span></a></li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+}
+</script>
+
+<style lang="scss">
+    .container {
+        width: 100%;
+        background-color: black;
+        color: white;
+        .main-content {
+            background-color: black;
+            h2 {
+                padding: 2em;
+            }
+        }
+        .main-end-menu {
+            background-color: #0282f9;
+            ul {
+                width: 80%;
+                margin: 0 auto;
+                padding: 1em;
+                display: flex;
+                align-items: center;
+                justify-content: space-around;
+                list-style: none;
+                li {
+                    img{
+                        width: 3em;
+                    }
+                    .location-logo{
+                        width: 2em;
+                    }
+                    span {
+                        font-size: 0.7em;
+                    }
+                    a {
+                        display: flex;
+                        align-items: center;
+                        text-decoration: none;
+                        color: white;
+                        gap: 0.5em;
+                    }
+                }
+            }
+        }
+    }
+</style>
